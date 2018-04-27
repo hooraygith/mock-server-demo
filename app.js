1af7f948c6b0c4a09c10a682f9aa21ac305c5758
Your Name <you@example.com>
2018-04-27 22:40:55 +0800

    Add upload file

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const Router = require('koa-router')
+const koaBody = require('koa-body')
 const app = new Koa()
 const router = new Router()
 
 let timer = 0
 
 router
-    .get('/v1/ucenter/wechat_qrcode/', (ctx, next) => {
-        timer += 1
-        if (timer > 2) {
-            ctx.body = {
-                "msg": "",
-                "errormsg": "账号已绑定过微信!",
-                "resultcode": 11406,
-                "data": {},
-                "result": false
-            }
-        } else {
-            ctx.body = {
-                "data": {
-                    "url": "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=gQFm7zwAAAAAAAAAAS5odHRwO"
-                },
-                "errormsg": "",
-                "msg": "",
-                "result": true,
-                "resultcode": 200
-            }
+    .get('/search/', (ctx, next) => {
+        const list = Array(20).fill(0).map(item => ({
+            name: '姓名' + parseInt(Math.random() * 100),
+            id: parseInt(Math.random() * 1000000000)
+        }))
+        ctx.body = {
+            "msg": "",
+            "errormsg": "",
+            data: {
+                objects: list,
+                total_count: 50,
+                limit: 20,
+                p: 1
+            },
+            "resultcode": 200,
+            "result": true
         }
 
     })
-    .post('/op/api/prize_receive/', (ctx, next) => {
+    .get('/op/api/prize_receive/', (ctx, next) => {
         ctx.body = {
             "msg": "",
             "errormsg": "wwwww!",
             "resultcode": 200,
-            "data": {is_success: true},
             "result": true
         }
+    })
+    .post('/upload', (ctx, next) => {
+
+
+
+        const file = ctx.request.body.files.key
+
+        console.log('received： %s -> %s', file.name);
+
+        ctx.body = {
+            "msg": "",
+            "errormsg": "",
+            "resultcode": 200,
+            "result": true
+        }
+
+        next()
+
     })
 
 
 // 跨域
-app.use(cors());
+app.use(cors())
+
+app.use(koaBody({multipart: true}))
 
 app.use(router.routes())
+
 
 app.listen(7777)
